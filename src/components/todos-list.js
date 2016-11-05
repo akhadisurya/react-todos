cc9e12a92fbe942ef036215e8a11aa1bc1e3d3bf
Arvin <akhadisurya@gmail.com>
2016-11-05 16:36:54 -0700

    modify app and todos list js files, and install lodash

--- src/components/todos-list.js
+++ src/components/todos-list.js
@@ -1,12 +1,21 @@
+import _ from 'lodash';
 import React from 'react';
 import TodosListHeader from './todos-list-header';
 
 export default class TodosList extends React.Component {
+  renderItems() {
+    return _.map(this.props.todos, (todo, index) => <TodosListItem key={index} {...todo} />);
+  }
+
+
   render() {
+
     return (
       <table>
-        <thead>
           <TodosListHeader />
+          <tr>
+            {this.renderItems()}
+          </tr>
       </table>
     )
   }
