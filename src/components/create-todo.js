293761129a9f24963283686fcaca3d13abefac6c
Arvin <akhadisurya@gmail.com>
2016-11-16 11:39:51 -0800

    create validateInput task in create todo js

--- src/components/create-todo.js
+++ src/components/create-todo.js
@@ -13,7 +13,17 @@ export default class TodosList extends React.Component {
   handleCreate(event){
     event.preventDefault();
 
+    const createInput = this.refs.createInput;
+    const task = createInput.value;
+    const validateInput = this.validateInput(task);
+
     this.props.createTask(this.refs.createInput.value);
     this.refs.createInput.value = '';
+  }
+
+  validateInput(task) {
+    if (!task) {
+      return 'Please Enter a Task';
+    } else if (_.find(this.props))
   }
 }
